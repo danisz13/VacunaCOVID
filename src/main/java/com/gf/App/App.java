ad6bc0c387283be0e0b5f8808d135a6333f12685
danisz13 <daniel9sanz9@gmail.com>
2021-06-14 09:55:55 +0200

    Proyecto final

--- src/main/java/com/gf/App/App.java
+++ src/main/java/com/gf/App/App.java
@@ -10,7 +10,6 @@ import com.gf.DAO.OrganizacionDAO;
 import com.gf.DAO.PaisDAO;
 import com.gf.DAO.UsuariosDAO;
 import com.gf.DAO.VacunaDAO;
-import com.gf.vista.DatosPais;
 import com.gf.vista.Login;
 
 /**
@@ -21,13 +20,11 @@ public class App {
 
     public static void main(String[] args) {
 
-        DatosPais vistaDatosPais = new DatosPais();
         Login loginView = new Login();
         OrganizacionDAO organizacionDAO = new OrganizacionDAO();
-        DatosPais datosPaisView= new DatosPais();
-        PaisDAO paisDAO=new PaisDAO();
-        VacunaDAO vacunaDAO=new VacunaDAO();
-        UsuariosDAO usuariosDAO=new UsuariosDAO();
+        PaisDAO paisDAO = new PaisDAO();
+        VacunaDAO vacunaDAO = new VacunaDAO();
+        UsuariosDAO usuariosDAO = new UsuariosDAO();
         ControladorVistaLogin controlador = new ControladorVistaLogin(loginView, organizacionDAO, paisDAO, vacunaDAO, usuariosDAO);
     }
 }
